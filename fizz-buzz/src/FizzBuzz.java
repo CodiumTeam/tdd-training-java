fff0cb29232129f16b6329aa80b149620cd192a3
Luis Rovirosa <luisrovirosa@gmail.com>
2015-06-16 08:34:45 +0200

    FizzBuzz

---
+++ fizz-buzz/src/FizzBuzz.java
@@ -0,0 +1,7 @@
+public class FizzBuzz {
+
+	public boolean renameMe() {
+		return true;
+	}
+
+}
---
+++ fizz-buzz/src/FizzBuzzTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FizzBuzzTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void renameMe() {
+		FizzBuzz fizzBuzz = new FizzBuzz();
+		assertTrue(fizzBuzz.renameMe());
+	}
+
+}
