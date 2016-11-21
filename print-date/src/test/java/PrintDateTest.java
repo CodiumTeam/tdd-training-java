e068fc31158dcc1e607fcfa9a78d9226a351f2a8
Luis Rovirosa <luisrovirosa@gmail.com>
2016-11-21 20:28:37 +0100

    maven print-date

--- print-date/src/test/java/PrintDateTest.java
+++ print-date/src/test/java/PrintDateTest.java
@@ -4,7 +4,9 @@ public class PrintDateTest {
 	@Test
 	public void printDate() throws Exception {
 		PrintDate printDate = new PrintDate();
+
 		printDate.printCurrentDate();
+
 		// How can we test this function?
 	}
 }
