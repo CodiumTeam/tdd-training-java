0bece2642c1a72f7b6eaedb3344791b1731e9539
Luis Rovirosa <luisrovirosa@gmail.com>
2016-11-21 20:21:27 +0100

    Maven gilded rose

--- gilded-rose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ gilded-rose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -10,7 +10,9 @@ public class GildedRoseTest {
     public void foo() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
+
         app.updateQuality();
+
         assertEquals("fixme", app.items[0].name);
     }
 
