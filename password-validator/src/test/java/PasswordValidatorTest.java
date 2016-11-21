f3ca91e92568f8d9d5a16fe046712b6d7fcae2da
Luis Rovirosa <luisrovirosa@gmail.com>
2016-11-21 20:25:52 +0100

    Maven password validator

---
+++ password-validator/src/test/java/PasswordValidatorTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PasswordValidatorTest {
+
+	@Test
+	public void renameMe() {
+		assertTrue(true);
+	}
+
+}
