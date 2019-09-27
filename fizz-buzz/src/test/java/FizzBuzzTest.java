209c9f5fcc07e6b59550d77aff35adc677ee66d1
Jordi Anguela <jordi.anguela@gmail.com>
2019-09-27 16:41:59 +0200

    fizz-buzz: update JUnit to 5.5.2

--- fizz-buzz/src/test/java/FizzBuzzTest.java
+++ fizz-buzz/src/test/java/FizzBuzzTest.java
@@ -1,6 +1,6 @@
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FizzBuzzTest {
 
