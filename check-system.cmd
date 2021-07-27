53a1d76f81ec7cb73905b5a25ffb6239d7c1a898
Jordi Anguela <jordi@codium.team>
2021-07-27 11:03:06 +0200

    add check-system files

---
+++ check-system.cmd
@@ -0,0 +1,50 @@
+@echo off
+
+CALL :validateDocker
+CALL :validateKata fizz-buzz "docker run --rm -v %CD%:/kata codiumteam/tdd-training-java make test"
+CALL :validateKata roman-numerals "docker run --rm -v %CD%:/kata codiumteam/tdd-training-java make test"
+CALL :validateKata password-validator "docker run --rm -v %CD%:/kata codiumteam/tdd-training-java make test"
+CALL :validateKata user-registration "docker run --rm -v %CD%:/kata codiumteam/tdd-training-java make test"
+CALL :validateKata coffee-machine "docker run --rm -v %CD%:/kata codiumteam/tdd-training-java make test"
+
+goto :eof
+
+:validateKata
+    echo Validating %1...
+    pushd %1
+    CALL %~2
+    popd
+goto :eof
+
+:validateDocker
+    echo Validating docker running...
+    docker ps >NUL: 2>NUL:
+    IF ERRORLEVEL 1 (
+      echo Error
+      echo Are you sure that you have docker running?
+      goto :eof
+    ) else (
+      echo "Ok"
+    )
+
+    echo Creating docker image...
+    docker build . -t codiumteam/tdd-training-java >NUL: 2>NUL:
+    IF ERRORLEVEL 1 (
+      echo Error
+      echo Do you have internet connection?
+      goto :eof
+    ) else (
+      echo Ok
+    )
+
+    echo Validating docker mount permissions...
+    docker run --rm -v "%CD%":/opt -w /opt codiumteam/tdd-training-java ls >NUL: 2>NUL:
+    IF ERRORLEVEL 1 (
+      echo Error
+      echo Are you sure that you have permissions to mount your volumes?
+      goto :eof
+    ) else (
+      echo Ok
+    )
+goto :eof
+
---
+++ check-system.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+ERROR=""
+OUTPUT=""
+function printStatus() {
+  if [ $? -ne 0 ]; then
+    echo "Error"
+    ERROR="${ERROR} \n\n${OUTPUT}"
+  else
+    echo "Ok"
+  fi
+}
+
+function validateKata() {
+    echo -n "Validating $1..."
+    OUTPUT=$($2 2>&1 && $3 2>&1 && $4 2>&1)
+    printStatus
+}
+
+function validateDocker() {
+    echo -n "Validating docker running..."
+    (docker ps) > /dev/null
+    if [ $? -ne 0 ]; then
+      echo "Error"
+      echo "Are you sure that you have docker running?"
+      echo "If you don't want to install docker, you can open tennis-refactoring-kata and run the tests using your IDE."
+      exit -1
+    else
+      echo "Ok"
+    fi
+
+    echo -n "Creating the docker image..."
+    (docker build . -t codium-tddtraining) > /dev/null
+    if [ $? -ne 0 ]; then
+      echo "Error"
+      echo "Do you have internet connection?"
+      exit -1
+    else
+      echo "Ok"
+    fi
+
+    echo -n "Validating docker mount permissions..."
+    (docker run --rm -v ${PWD}:/kata codium-tddtraining ls) > /dev/null
+    if [ $? -ne 0 ]; then
+      echo "Error"
+      echo "Are you sure that you have permissions to mount your volumes?"
+      exit -1
+    else
+      echo "Ok"
+    fi
+}
+
+function validateMake() {
+  echo -n "Validating make installed..."
+    (make -h) > /dev/null
+    if [ $? -ne 0 ]; then
+      echo "Error"
+      echo "Do you have make installed?"
+      echo "If you don't want to install make, you can run read how to run the tests opening the Makefile."
+      exit -1
+    else
+      echo "Ok"
+    fi
+}
+
+validateDocker
+validateMake
+validateKata fizz-buzz "cd fizz-buzz" "make docker-test"
+validateKata roman-numerals "cd roman-numerals" "make docker-test"
+validateKata password-validator "cd password-validator" "make docker-test"
+validateKata user-registration "cd user-registration" "make docker-test"
+validateKata coffee-machine "cd coffee-machine" "make docker-test"
+
+if [ -z "$ERROR" ]; then
+  echo "Congratulations! You are ready for the training!"
+else
+  echo -e "----------------------------------------------------------\n\n$ERROR"
+  echo -e "\n\nPlease send an email with the problem you have to info@codium.team\n"
+fi
