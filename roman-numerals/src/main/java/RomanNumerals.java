89c2ad3787840b4211701eb4732258fcf76c5424
Luis Rovirosa Mairlot <luis@codium.team>
2021-11-16 15:49:33 +0100

    [roman-numerals] remove duplication

--- roman-numerals/src/main/java/RomanNumerals.java
+++ roman-numerals/src/main/java/RomanNumerals.java
@@ -3,20 +3,10 @@ public class RomanNumerals {
         if (decimal <= 0) {
             return "";
         }
-        if (decimal == 1) {
+        if (decimal <= 3) {
             return "I" + toRoman(decimal - 1);
-        } else if (decimal == 2 ){
-            return "I" + toRoman(decimal - 1);
-        } else if (decimal == 3) {
-            return "I" + toRoman(decimal - 1);
-        } else if (decimal == 5) {
+        } else if (decimal <= 8) {
             return "V" + toRoman(decimal - 5);
-        } else if (decimal == 6) {
-            return "V" + toRoman(decimal-5);
-        } else if (decimal == 7) {
-            return "V" + toRoman(decimal-5);
-        } else if (decimal == 8) {
-            return "V" + toRoman(decimal-5);
         }
         return null;
     }
