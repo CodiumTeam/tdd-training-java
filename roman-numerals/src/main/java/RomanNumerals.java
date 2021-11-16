a883d497a44ce8116e66ef919e8021d65cf37246
Luis Rovirosa Mairlot <luis@codium.team>
2021-11-16 15:46:27 +0100

    [roman-numerals] improve symmetry with duplication

--- roman-numerals/src/main/java/RomanNumerals.java
+++ roman-numerals/src/main/java/RomanNumerals.java
@@ -1,13 +1,13 @@
 public class RomanNumerals {
     public String toRoman(int decimal) {
         if (decimal == 1) {
-            return "I";
+            return "I" + toRoman(0);
         } else if (decimal == 2 ){
             return "I" + toRoman(1);
         } else if (decimal == 3) {
             return "I" + toRoman(2);
         } else if (decimal == 5) {
-            return "V";
+            return "V" + toRoman(0);
         } else if (decimal == 6) {
             return "V" + toRoman(1);
         } else if (decimal == 7) {
@@ -15,6 +15,6 @@ public class RomanNumerals {
         } else if (decimal == 8) {
             return "V" + toRoman(3);
         }
-        return null;
+        return "";
     }
 }
