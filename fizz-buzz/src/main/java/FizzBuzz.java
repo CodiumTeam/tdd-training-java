4f9eb8da14000171e0e22ac7a52faf3f284d1610
Luis Rovirosa Mairlot <luis@codium.team>
2023-04-17 12:38:09 +0200

    return_100_values

---
+++ fizz-buzz/src/main/java/FizzBuzz.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+    public List<String> values() {
+        ArrayList<String> values = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            values.add(null);
+        }
+        return values;
+    }
+}
--- fizz-buzz/src/test/java/FizzBuzzTest.java
+++ fizz-buzz/src/test/java/FizzBuzzTest.java
@@ -1,12 +1,19 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FizzBuzzTest {
 
 	@Test
-	public void renameMe() {
-		assertTrue(true);
+	public void return_100_values() {
+		FizzBuzz fizzBuzz = new FizzBuzz();
+
+		List<String> values = fizzBuzz.values();
+
+		assertEquals(100, values.toArray().length);
 	}
 
 }
