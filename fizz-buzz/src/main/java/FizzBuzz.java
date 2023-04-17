3de7521622868f9ea07e0e84f9800ebdada9af6a
Luis Rovirosa Mairlot <luis@codium.team>
2023-04-17 12:41:33 +0200

    refactor reduce duplicity

--- fizz-buzz/src/main/java/FizzBuzz.java
+++ fizz-buzz/src/main/java/FizzBuzz.java
@@ -5,13 +5,7 @@ public class FizzBuzz {
     public List<String> values() {
         ArrayList<String> values = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
-            if (i == 0) {
-                values.add("1");
-            } else if (i == 1) {
-                values.add("2");
-            } else {
-                values.add("4");
-            }
+            values.add(String.valueOf(i + 1));
         }
         return values;
     }
