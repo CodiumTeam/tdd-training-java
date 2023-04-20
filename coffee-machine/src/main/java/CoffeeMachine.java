5f721580aa14fc1602c2f2764fd84807375438c6
Luis Rovirosa Mairlot <luis@codium.team>
2023-04-20 11:28:19 +0200

    [coffee-machine] prepare_a_coffee_without_sugar

--- coffee-machine/src/main/java/CoffeeMachine.java
+++ coffee-machine/src/main/java/CoffeeMachine.java
@@ -1,2 +1,11 @@
 public class CoffeeMachine {
+    private final DrinkMaker drinkMaker;
+
+    public CoffeeMachine(DrinkMaker drinkMaker) {
+        this.drinkMaker = drinkMaker;
+    }
+
+    public void prepareCoffee() {
+        drinkMaker.execute("C::");
+    }
 }
--- coffee-machine/src/test/java/CoffeeMachineTest.java
+++ coffee-machine/src/test/java/CoffeeMachineTest.java
@@ -1,10 +1,18 @@
 import org.junit.jupiter.api.Test;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
 public class CoffeeMachineTest {
 
     @Test
-    public void think_a_good_name_and_rename_this_method() {
-        new CoffeeMachine();
+    public void prepare_a_coffee_without_sugar() {
+        DrinkMaker drinkMaker = mock(DrinkMaker.class);
+        CoffeeMachine coffeeMachine = new CoffeeMachine(drinkMaker);
+
+        coffeeMachine.prepareCoffee();
+
+        verify(drinkMaker).execute("C::");
     }
 
 
