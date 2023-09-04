0d2c5be1ee84b388078dbc2889c09c0171d52710
Luis Rovirosa Mairlot <luis@codium.team>
2023-09-04 12:45:03 +0200

    the_first_element_is_1

---
+++ fizz-buzz/src/main/java/FizzBuzz.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+    public List<String> execute() {
+        ArrayList<String> numbers = new ArrayList<>();
+        numbers.add("1");
+        return numbers;
+    }
+}
--- fizz-buzz/src/test/java/FizzBuzzTest.java
+++ fizz-buzz/src/test/java/FizzBuzzTest.java
@@ -1,12 +1,18 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
 
 	@Test
-	public void renameMe() {
-		assertTrue(true);
+	public void the_first_element_is_1() {
+		FizzBuzz fizzBuzz = new FizzBuzz();
+
+		List<String> numbers = fizzBuzz.execute();
+
+		assertEquals("1", numbers.get(0));
 	}
 
 }
